2be37916c2573545b036de7e6a8ba10cc2f6cf7b
mgmarltd <gencehanarpacay@gmail.com>
2021-03-21 04:29:53 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,361 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>KILIÇKAYA HUKUK BÜROSU</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,700,800" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="top-menu">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-2">
+						<div id="fh5co-logo"><a href="index.html">KILIÇKAYA<span>.</span></a></div>
+					</div>
+					<div class="col-xs-10 text-right menu-1">
+						<ul>
+							<li class="active"><a href="index.html">Anasayfa</a></li>
+							<li><a href="#about">Hakkımızda</a></li>
+							<li><a href="#work">Çalışma Alanlarımız</a></li>
+							<li><a href="#contact">İletişim</a></li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</nav>
+
+	<aside id="fh5co-hero" class="js-fullheight">
+		<div class="flexslider js-fullheight">
+			<ul class="slides">
+		   	<li style="background-image: url(images/t1.jpg);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="row">
+			   			<div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+			   				<div class="slider-text-inner">
+			   					<h1>BAKILAN DAVALAR VE ÇALIŞMA ALANLARI</h1>
+									<p><a class="btn btn-primary btn-lg" href="tel:+905322461286">Detaylı Bilgi için Bizi Arayın</a></p>
+
+			   				</div>
+			   			</div>
+			   		</div>
+		   		</div>
+		   	</li>
+		   	<li style="background-image: url(images/t2.jpg);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="row">
+			   			<div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+			   				<div class="slider-text-inner">
+			   					<h1>İHRACATA YÖNELİK DEVLET YARDIMLARI</h1>
+									<p><a class="btn btn-primary btn-lg" href="tel:+905322461286">Detaylı Bilgi için Bizi Arayın</a></p>
+			   				</div>
+			   			</div>
+			   		</div>
+		   		</div>
+		   	</li>
+		   	<li style="background-image: url(images/t3.jpg);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="row">
+			   			<div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+			   				<div class="slider-text-inner">
+			   					<h1>HUKUKİ DANIŞMANLIK</h1>
+									<p><a class="btn btn-primary btn-lg" href="tel:+905322461286">Detaylı Bilgi için Bizi Arayın</a></p>
+			   				</div>
+			   			</div>
+			   		</div>
+		   		</div>
+		   	</li>		   	
+		  	</ul>
+	  	</div>
+	</aside>
+
+	
+	<div id="fh5co-content">
+		<div class="video fh5co-video" style="background-image: url(images/a2.webp);">
+		</div>
+		<div class="choose animate-box" id="about">
+			<div class="fh5co-heading">
+				<h2>Hüsametdin Kılıçkaya Kimdir?</h2>
+				<p>A.Ü. Siyasal Bilgiler Fakültesi (1980) ve A.Ü. Hukuk Fakültesi (2019) mezunudur. Ticaret Bakanlığında(daha sonra sırasıyla Hazine ve Dış Ticaret Müsteşarlığı, Dışticaret Müsteşarlığı, Ekonomi Bakanlığı ve Ticaret Bakanlığı isimlerini almıştır) uzman yardımcısı olarak başladığı memuriyet hayatına yurt içinde  muhtelif kademelerde (Şube Müdürü, Daire Başkanı, Genel Müdür Yardımcısı ve Genel Müdür Vekilliği), yurtdışında da muhtelif ülkelerde Ticaret Ataşeliği görevlerinde bulunmuştur. 2019 yılında memuriyet hayatını sonlandıran Kılıçkaya, 2020 yılında Ankara barosuna kayıtlı avukat olarak mesleğe başlamıştır.</p>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-practice" class="fh5co-bg-section">
+		<div class="container">
+			<div class="row animate-box" id="work">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Çalıştığımı Hukuk Alanları</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-umbrella"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">İş ve Sosyal Güvenlik Hukuku</a></h3>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-price-ribbon"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Borçlar Hukuku</a></h3>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-help"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Ceza Hukuku</a></h3>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+			<div class="row">
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-home"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Gayrimenkul Hukuku ve Yönetimi</a></h3>
+							<p>Taşınmazların Kira Yönetimi, Kira Bedelinin Tespiti Davaları Tahliye Davaları, Kat Mülkiyeti Kanunundan Kaynaklanan Uyuşmazlıklar</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-users"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Aile Hukuku</a></h3>
+							<p>Boşanma Davaları, yurt dışında yaşayan Türk vatandaşlarının yabancı ülke mahkemelerinden aldıkları boşanma kararlarının tanınması ve tenfizi, aile konutu şerhinin tapu kütüğüne işlenmesi </p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-map"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Miras Hukuku </a></h3>
+							<p>Taşınmaz intikali, yabancı ülke mahkemelerinden alınan  veraset ilamının tanınması ve tenfizi, mahkemeden veraset ilamı alınması, tapuda işlem yapılması taşınmazlardaki hak ve hisselerin  mirasçılık belgesine göre intikali, elbirliği halindeki mülkiyetin paylı mülkiyete dönüştürülmesi</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	
+	<div id="fh5co-project">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Danışmanlık</h2>
+					<p>Büromuz, hukuki dava ve işlerin yanında, Ticaret Bakanlığı tarafından yürütülmekte olan <b>ihracata yönelik devlet yardımları</b> (Marka ve Turquality Desteği, Yurtdışı Birim, Marka ve Tanıtım Desteği, Pazara Giriş Belgelerinin Desteklenmesi, Pazar Araştırması ve Pazara Giriş Desteği, Sektörel Nitelikli Uluslararası Yurt İçi Fuarların Desteklenmesi, Uluslararası Rekabetçiliğin Geliştirilmesinin Desteklenmesi, Tasarım Desteği, Döviz Kazandırıcı Hizmet Ticareti Desteği, Döviz Kazandırıcı Hizmet Sektörleri Markalaşma Desteği, Pazara Girişte Dijital Faaliyetlerin Desteklenmesi) ve <b>Dahilde İşleme Rejimi</b> konularında <b>şirketlere</b>; deneyimli avukatımız Hüsametdin Kılıçkaya öncülüğünde danışmanlık hizmeti de (Başvuru dosyasının hazırlanması, ihracatçı birliklerine intikali, Ticaret Bakanlığı, Ticaret Müşavirlikleri/Ataşelikleri nezdinde takibi) <b>vermektedir.</b></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="rowhus">
+	<h2><b>İHRACATA YÖNELİK DEVLET YARDIMLARI</b></h2>
+	</div>
+	<br/>
+	<div class="rowhus col-md-12">
+	<div class="container">
+		<div class"col-md-8 col-md-offset-2 text-center">
+			<h2>Mal İhracatı Yapan Sektörlere İlişkin Destekler</h2>
+			<p>Mal ihracatı yapan ve yurtdışına açılma sürecinde olan şirketlere  destek sağlamak amacıyla  geliştirilen İhracata Yönelik Devlet Yardımları;  ihracata hazırlık, pazarlama ve markalaşma olmak üzere üç aşamayı kapsamaktadır</p>
+		</div>
+	</div>
+	<div class="container">
+		<div class"col-md-4">
+			<h2>Hizmet İhracatı Yapan Sektörlere Yönelik Destekler</h2>
+			<p>Mal ihracatının yanı sıra hizmet ihracatı da devlet tarafından desteklenen sektörler arasında yer almakta ve bu çerçevede 2011 yılından itibaren hizmet sektörlerine yönelik Döviz Kazandırıcı Hizmet Ticareti Desteği, Döviz Kazandırıcı Hizmet Sektörleri Markalaşma Desteği, Pazara Girişte Dijital Faaliyetlerin Desteklenmesi adları altında  kapsamlı devlet yardım programları uygulanmaktadır. </p>
+
+		</div>
+	</div>	
+	</div>
+	
+	
+
+	<div id="fh5co-testimonial" class="fh5co-bg-section">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2><b>DAHİLDE İŞLEME REJİMİ</b></h2>
+				</div>
+			</div>
+			<div class="row text-center">
+			<p>Türk İhraç ürünlerine dünya piyasalarında rekabet gücü kazandırmak ve ihraç ürünlerimizi çeşitlendirmek amacıyla, dünya piyasa fiyatlarından gümrük muafiyetli olarak, ticaret politikası önlemlerine tabi olmaksızın, ihraç ürünün üretimi için gerekli olan ve fiyat ve/veya kalite bakımından yurt içi piyasalardan temin edilemeyen; hammadde, yardımcı madde ve ambalaj malzemeleri ithalatına imkan veren ekonomik etkili bir gümrük rejimidir. </p>	
+			<br/>
+			<p>Dahilde İşleme Rejiminde; ‘Şartlı Muafiyet’ ve ‘Geri Ödeme Sistemi’ olmak üzere; iki tür dahilde işleme tedbiri uygulanmakta olup şartlı muafiyet sisteminde; ithalat sırasında doğan vergilerin teminata bağlanırken, geri ödeme sisteminde; ithalat esnasında vergi alınmakta, işlem görmüş ürünün ihracı halinde ise alınan vergi iade edilmektedir. </p>
+		</div>
+		<br/>
+		<br/>
+
+		<br/>
+
+		<br/>
+
+	</div>
+
+	
+
+	
+	
+
+	<div id="fh5co-started" style="background-image:url(images/img_bg_2.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>DAHİLDE İŞLEME REJİMİ</h2>
+					<p>Türk İhraç ürünlerine dünya piyasalarında rekabet gücü kazandırmak ve ihraç ürünlerimizi çeşitlendirmek amacıyla, dünya piyasa fiyatlarından gümrük muafiyetli olarak, ticaret politikası önlemlerine tabi olmaksızın, ihraç ürünün üretimi için gerekli olan ve fiyat ve/veya kalite bakımından yurt içi piyasalardan temin edilemeyen; hammadde, yardımcı madde ve ambalaj malzemeleri ithalatına imkan veren ekonomik etkili bir gümrük rejimidir.
+</p><p>
+Dahilde İşleme Rejiminde; ‘Şartlı Muafiyet’ ve ‘Geri Ödeme Sistemi’ olmak üzere; iki tür dahilde işleme tedbiri uygulanmakta olup şartlı muafiyet sisteminde; ithalat sırasında doğan vergilerin teminata bağlanırken, geri ödeme sisteminde; ithalat esnasında vergi alınmakta, işlem görmüş ürünün ihracı halinde ise alınan vergi iade edilmektedir
+</p>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<p><a href="tel:+905322461286" class="btn btn-default btn-lg">Danışmanlık İçin Bizi Arayın</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+			<div class="row row-pb-md">
+				
+				<div class="col-md-3 col-md-push-1">
+					<h4>Sitede Yönlendirme</h4>
+					<ul class="fh5co-footer-links">
+						<li><a href="#about">Anasayfa</a></li>
+						<li><a href="#about">Hakkımızda</a></li>
+						<li><a href="#work">Çalışma Alanlarımız</a></li>
+					</ul>
+				</div>
+
+				<div class="col-md-3 col-md-push-1" id="contact">
+					<h4>İletişim Bilgilerimiz</h4>
+					<ul class="fh5co-footer-links">
+						<li>Mebusevleri Mahallesi Turgut Reis Caddesi, 4/3 06570 Çankaya/Ankara</li>
+						<li><a href="tel:+905322461286">+90 532 246 1286</a></li>
+						<li><a href="mailto:avkilickaya@gmail.com">avkilickaya@gmail.com</a></li>
+					</ul>
+				</div>
+
+			
+
+			</div>
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2021 Tüm Hakları Saklıdır</small> 
+						<small class="block">Bu Websitesi <a href="http://cyberarp.co/" target="_blank"> CYBERARP.CO Tarafından tasarlanmıştır</a> 
+					</p>
+					
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
