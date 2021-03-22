233695b27873a3d355c2c403c552ae04d900d98f
mgmarltd <gencehanarpacay@gmail.com>
2021-03-22 16:57:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,10 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-2">
-						<div id="fh5co-logo"><a href="index.html"><img src="images/logo.png"></a></div>
+						<div id="fh5co-logo"><a href="index.html"><img src="images/logo.png" style="
+    width: 350px;
+    object-fit: contain;
+"></a></div>
 					</div>
 					<div class="col-xs-10 text-right menu-1">
 						<ul>
